53da8a05f66000ab4b57f198f348be981f849f48
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-04 20:22:29 -0400

    chore(init): reorder hooks per hook_api.md lifecycle order

--- init.zsh
+++ init.zsh
@@ -1,11 +1,5 @@
 # shellcheck shell=bash
 ######################################################################
-#<
-#
-# Function: p6df::modules::rails::deps()
-#
-#>
-######################################################################
 p6df::modules::rails::deps() {
   ModuleDeps=(
     p6m7g8-dotfiles/p6df-ruby
@@ -13,26 +7,6 @@ p6df::modules::rails::deps() {
   )
 }
 
-######################################################################
-#<
-#
-# Function: p6df::modules::rails::vscodes()
-#
-#>
-######################################################################
-p6df::modules::rails::vscodes() {
-
-  p6df::modules::vscode::extension::install bung87.rails
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::rails::mcp()
-#
-#>
 ######################################################################
 p6df::modules::rails::mcp() {
 
@@ -45,6 +19,37 @@ p6df::modules::rails::mcp() {
 }
 
 ######################################################################
+p6df::modules::rails::vscodes() {
+
+  p6df::modules::vscode::extension::install bung87.rails
+
+  p6_return_void
+}
+
+######################################################################
+p6df::modules::rails::profile::mod() {
+
+  p6_return_words 'rails' '$RAILS_ENV'
+}
+######################################################################
+#<
+#
+# Function: p6df::modules::rails::deps()
+#
+#>
+######################################################################
+#<
+#
+# Function: p6df::modules::rails::vscodes()
+#
+#>
+######################################################################
+#<
+#
+# Function: p6df::modules::rails::mcp()
+#
+#>
+######################################################################
 #<
 #
 # Function: words rails $RAILS_ENV = p6df::modules::rails::profile::mod()
@@ -54,8 +59,3 @@ p6df::modules::rails::mcp() {
 #
 #  Environment:	 RAILS_ENV
 #>
-######################################################################
-p6df::modules::rails::profile::mod() {
-
-  p6_return_words 'rails' '$RAILS_ENV'
-}
